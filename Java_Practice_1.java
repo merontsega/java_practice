77b5d1d7e2b6a39e2bbf907572db7d5ce7a03ffc
merontsega <merontsega25@gmail.com>
2024-04-15 19:59:24 -0400

    Java practice homework

---
+++ Java_Practice_1.java
@@ -0,0 +1,50 @@
+package com.practicetime1;
+
+public class Java_Practice_1 {
+
+	public static void main(String[] args) {
+		
+		// 1. Write a code that prints whole numbers greater than 10 and less than 100.
+		
+		//while loop
+//		int i = 11;
+//		while (i < 100) {System.out.println(i);
+//		i++;
+//		}
+//		
+//		//for loop
+//		for (int t = 11; t <100; t++) {
+//		System.out.println(t);
+//		}
+//		
+		// for loop with if 
+		for (int l = 10; l < 100; l++) 
+			if(l%2 == 1)
+			System.out.println(l);
+		
+//		// 2. Write a code that prints the following sequences of numbers
+//		//(prints even values between 0 and 12, inclusive)
+//
+//		for (int l = 0; l <= 12; l++) 
+//		if(l%2==0)
+//		System.out.println(l);
+//		
+//		// 3. Write a code that displays all odd numbers from 1 to 100.
+//		// Don’t use an if statement here.
+//	  
+//		for (int f=1; f<=100; f +=2)
+//		{System.out.println(f);}
+//		
+//		// 4. Write a code that displays the sum of natural numbers 
+//		//from 1 to 10.
+//		
+//		int sum = 0;
+//		for (int j = 1; j <= 10; j++) {sum = sum + j;}
+//		System.out.println(sum);
+		
+		
+		
+		
+	}
+
+}
---
+++ Java_Practice_2.java
@@ -0,0 +1,40 @@
+package com.practicetime1;
+
+import java.util.Scanner;
+
+public class Java_Practice_2 {
+
+	public static void main(String[] args) {
+		
+  //Question 1: Write a program that prints numbers from 1 to 10 
+	//using a for loop.
+		
+//		for (int i = 1; i <= 10; i++) {System.out.println(i);}
+//	
+//Question 2: Write a program to find the sum of all numbers from 
+//1 to 100 using a for loop.
+//		
+//		int sum = 0;
+//		for (int f = 1; f <=100; f++) {sum +=f;}
+//		System.out.println("sum:" + sum);
+//		
+//Question 3: Write a program that prints the multiplication table 
+//of a number entered by the user using a for loop.
+
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("Enter a number:" );
+//		 int num = scanner.nextInt();
+//		 scanner.close();
+//		 for (int l = 1; l <= 10; l++) {
+//		 Sout(num + " x " + l + " = " + (num * l));
+//		 
+////
+		
+	}
+
+	private static void Sout(String string) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
